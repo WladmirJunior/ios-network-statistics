65450c2ae64f8466f95c83cc313452817fc6c245
Wladmir Silva <wladmir.silva@nubank.com.br>
2023-05-24 18:24:24 -0300

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Wladmir Silva on 24/05/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
