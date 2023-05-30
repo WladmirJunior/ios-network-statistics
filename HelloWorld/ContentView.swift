c657e1c2e7cb65fcf6823d90484e2f6c129a5c84
Wladmir Silva <wladmir.silva@nubank.com.br>
2023-05-30 11:08:08 -0300

    Up

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -8,12 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let dataUsage: DataUsageInfo
+    
+    init() {
+        dataUsage = DataUsage.getDataUsage()
+        print(dataUsage)
+    }
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Data state:")
+                .padding(.vertical)
+            Button(role: .none) {
+                print("Wifi Received: \(dataUsage.wifiReceived)")
+            } label: {
+                Text("Wifi Received")
+            }
+            Button(role: .none) {
+                print("Wifi Sent: \(dataUsage.wifiSent)")
+            } label: {
+                Text("Wifi Sent")
+            }
+            Button(role: .none) {
+                print("Wan Received: \(dataUsage.wirelessWanDataReceived)")
+            } label: {
+                Text("Wan Received")
+            }
+            Button(role: .none) {
+                print("Wan Sent: \(dataUsage.wirelessWanDataSent)")
+            } label: {
+                Text("Wan Sent")
+            }
         }
         .padding()
     }
@@ -22,5 +51,75 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+struct DataUsageInfo {
+    var wifiReceived: UInt32 = 0
+    var wifiSent: UInt32 = 0
+    var wirelessWanDataReceived: UInt32 = 0
+    var wirelessWanDataSent: UInt32 = 0
+
+    mutating func updateInfoByAdding(info: DataUsageInfo) {
+        wifiSent += info.wifiSent
+        wifiReceived += info.wifiReceived
+        wirelessWanDataSent += info.wirelessWanDataSent
+        wirelessWanDataReceived += info.wirelessWanDataReceived
+    }
+}
+
+class DataUsage {
+
+    private static let wwanInterfacePrefix = "pdp_ip"
+    private static let wifiInterfacePrefix = "en"
+
+    class func getDataUsage() -> DataUsageInfo {
+        var interfaceAddresses: UnsafeMutablePointer<ifaddrs>? = nil
+
+        var dataUsageInfo = DataUsageInfo()
+
+        guard getifaddrs(&interfaceAddresses) == 0 else { return dataUsageInfo }
+
+        var pointer = interfaceAddresses
+        while pointer != nil {
+            guard let info = getDataUsageInfo(from: pointer!) else {
+                pointer = pointer!.pointee.ifa_next
+                continue
+            }
+            dataUsageInfo.updateInfoByAdding(info: info)
+            pointer = pointer!.pointee.ifa_next
+        }
+
+        freeifaddrs(interfaceAddresses)
+
+        return dataUsageInfo
+    }
+
+    private class func getDataUsageInfo(from infoPointer: UnsafeMutablePointer<ifaddrs>) -> DataUsageInfo? {
+        let pointer = infoPointer
+
+        let name: String! = String(cString: infoPointer.pointee.ifa_name)
+        let addr = pointer.pointee.ifa_addr.pointee
+        guard addr.sa_family == UInt8(AF_LINK) else { return nil }
+
+        return dataUsageInfo(from: pointer, name: name)
+    }
+
+    private class func dataUsageInfo(from pointer: UnsafeMutablePointer<ifaddrs>, name: String) -> DataUsageInfo {
+        var networkData: UnsafeMutablePointer<if_data>? = nil
+        var dataUsageInfo = DataUsageInfo()
+        
+        if name.hasPrefix(wifiInterfacePrefix) {
+            networkData = unsafeBitCast(pointer.pointee.ifa_data, to: UnsafeMutablePointer<if_data>.self)
+            dataUsageInfo.wifiSent += networkData?.pointee.ifi_obytes ?? 0
+            dataUsageInfo.wifiReceived += networkData?.pointee.ifi_ibytes ?? 0
+        } else if name.hasPrefix(wwanInterfacePrefix) {
+            networkData = unsafeBitCast(pointer.pointee.ifa_data, to: UnsafeMutablePointer<if_data>.self)
+            dataUsageInfo.wirelessWanDataSent += networkData?.pointee.ifi_obytes ?? 0
+            dataUsageInfo.wirelessWanDataReceived += networkData?.pointee.ifi_ibytes ?? 0
+        }
+
+        return dataUsageInfo
     }
 }
